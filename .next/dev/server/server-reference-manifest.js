a7654ca208a890efa2cbb6855888561796bcbb8e
Walee <walee@Ahsan-Tahir-MacBook-Pro.local>
2026-01-18 01:11:21 +0500

    chore: Remove .next build artifacts from git and update .gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qfhGeJyTOnDj+hoeD+CusrMLvoUYLqig77P2eZ/n088=\"\n}"
--- .next/dev/static/chunks/app_matches_page_tsx_a8c03fea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_0ab67cb9._.js",
-  "static/chunks/_47f144ff._.js"
-],
-    source: "dynamic"
-});
